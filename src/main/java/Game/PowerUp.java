2ad73ad107287f3f8b5bc2cc699aeb8be88ea3e9
Hoang-Long-356 <24020213@vnu.edu.vn>
2025-10-14 22:46:51 +0700

    Thêm code cơ bản của PowerUp

--- src/main/java/Game/PowerUp.java
+++ src/main/java/Game/PowerUp.java
@@ -1,22 +1,34 @@
 package Game;
 
-import javafx.scene.canvas.GraphicsContext;
-
 public abstract class PowerUp extends GameObject {
-    protected String type;
-    protected int duration;
 
-    public PowerUp(int x, int y, int width, int height, String type, int duration) {
+    protected String type;
+    protected double duration; // int -=> double
+    protected double dy = 2.0; // tốc độ trơi
+
+    public PowerUp(int x, int y, int width, int height, String type, double duration) {
         super(x, y, width, height);
         this.type = type;
         this.duration = duration;
+    }
+
+    public String getType() {
+        return type;
+    }
+    public double getDuration() {
+        return  duration;
     }
 
     public abstract void applyEffect(Paddle paddle);
     public abstract void removeEffect(Paddle paddle);
 
     @Override
-    public void update() {}
+    public void update() {
+        this.y += dy;
+    }
+
     @Override
-    public void render(GraphicsContext gc) {}
+    public void render() {
+        // thêm ảnh ở đây
+    }
 }
