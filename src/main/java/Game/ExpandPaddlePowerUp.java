26e16c3b2c95d277614ffa5250d4195c4d94c79c
Hoang-Long-356 <24020213@vnu.edu.vn>
2025-10-19 22:42:26 +0700

    Fix lỗi PowerUp

--- src/main/java/Game/ExpandPaddlePowerUp.java
+++ src/main/java/Game/ExpandPaddlePowerUp.java
@@ -3,6 +3,7 @@ package Game;
 public class ExpandPaddlePowerUp extends PowerUp {
     private final int deltaWidth = 40;
     private boolean isActive = false;
+    private int originalWidth; // lưu width gốc
 
     public ExpandPaddlePowerUp(int x, int y) {
         super(x, y, 20, 20, "ExpandPaddle", 10); // kích thước 20x20, 10s
@@ -12,14 +13,22 @@ public class ExpandPaddlePowerUp extends PowerUp {
     public void applyEffect(Paddle paddle) {
         if (isActive) return;
         isActive = true;
-        paddle.setWidth(paddle.getWidth() + deltaWidth);
 
+        originalWidth = paddle.getWidth();
+        paddle.width = originalWidth + deltaWidth;
+
+        if (paddle.x < 0) paddle.x = 0;
+        if (paddle.x + paddle.width > 800) paddle.x = 800 - paddle.width;
     }
 
     @Override
     public void removeEffect(Paddle paddle) {
         if (!isActive) return;
         isActive = false;
-        paddle.setWidth(Math.max(20, paddle.getWidth() - deltaWidth));
+
+        paddle.width = originalWidth;
+
+        if (paddle.x < 0) paddle.x = 0;
+        if (paddle.x + paddle.width > 800) paddle.x = 800 - paddle.width;
     }
 }
